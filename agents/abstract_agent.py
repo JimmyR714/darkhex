4e5149b2052fbf12cd2fc87cdd502c061bd5eae6
JimmyR714 <jimmyr7141@gmail.com>
2025-04-04 15:52:19 +0100

    Functional Bayesian Network setup

--- agents/abstract_agent.py
+++ agents/abstract_agent.py
@@ -4,7 +4,9 @@ The imperfect aspect is included using a belief system that the agent never sees
 """
 
 from agents.agent import Agent
-from pgmpy.models import DiscreteBayesianNetwork
+from pgmpy.models import FunctionalBayesianNetwork
+from pgmpy.factors.hybrid import FunctionalCPD
+import pyro.distributions as dist
 
 
 class AbstractAgent(Agent):
@@ -18,6 +20,8 @@ class AbstractAgent(Agent):
         """
         super().__init__(num_cols, num_rows, settings)
         # assign all settings here
+        self.cell_networks = self.initialise_networks()
+        self.weightings = settings["weightings"]
 
 
     def move(self):
@@ -35,13 +39,30 @@ class AbstractAgent(Agent):
 
 
     def reset(self):
+        self.cell_networks = self.initialise_networks()
         return super().reset()
 
 
-    def initialise_network(self):
+    def initialise_networks(self) -> list[list[FunctionalBayesianNetwork]]:
         """
-        Create the base Bayesian Network that contains our beliefs
+        Create the Bayesian Networks that contains our beliefs
         """
+        # create a network centered around each node
+        for row in range(self.num_rows):
+            for col in range(self.num_cols):
+                total_weighting = 0
+                # first, create our model using only cpds based on distance from us
+                if self.weightings["width_1"] > 0:
+                    # add weightings of all cells within 1 distance
+                    width_1_cpd = FunctionalCPD(
+                        variable=(col, row),
+                        fn=self.weightings["width_1"], # add function here
+                        parents=[] #add parents here
+                    )
+                if self.weightings["width_2"] > 0:
+                    # add weightings of all cells within 2 distance
+                    width_2_cpd = FunctionalCPD()
+                
 
 # For the hex AI, we require some way of returning value of the move,
 # ideally the value of multiple moves to add to a large expected value
