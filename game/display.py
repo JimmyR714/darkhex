5943a385d5b75ccd444ec678b4a73a6e9c29a125
JimmyR714 <jimmyr7141@gmail.com>
2025-05-08 17:43:42 +0100

    Added training frame

--- game/display.py
+++ game/display.py
@@ -35,11 +35,13 @@ class DisplayWindow(tk.Tk):
         self.won = False
         self.win_title = tk.Label(self, text="")
         self.win_title.grid(row=1, column=1, columnspan=3)
+        self.training_mode = False
 
         # add key frames
         self.main_menu = MainMenuFrame(self)
         self.game_frames : list[tk.Frame] = []
         self.num_game_frames = 0
+        self.training_frame = None
 
         # grid frames
         self.fullscreen = True
@@ -74,62 +76,95 @@ class DisplayWindow(tk.Tk):
         First gets the selected game settings from the main menu.
         Then it creates a new game, with the correct sizes and game frames.
         """
-        # get rows and cols
-        rows = self.main_menu.rows
-        cols = self.main_menu.cols
+        if not self.training_mode:
+            # get rows and cols
+            rows = self.main_menu.rows
+            cols = self.main_menu.cols
 
-        # get the required displays
-        displays = []
-        if self.main_menu.global_display.get() == 1:
-            displays.append("global")
-        if self.main_menu.white_display.get() == 1:
-            displays.append("white")
-        if self.main_menu.black_display.get() == 1:
-            displays.append("black")
+            # get the required displays
+            displays = []
+            if self.main_menu.global_display.get() == 1:
+                displays.append("global")
+            if self.main_menu.white_display.get() == 1:
+                displays.append("white")
+            if self.main_menu.black_display.get() == 1:
+                displays.append("black")
 
-        #reset previous game frames
+            #reset previous game frames
+            self.kill_game()
+            #set the number of game frames to be created for correct sizing
+            self.num_game_frames = len(displays)
+            #create new game frames
+            for d in displays:
+                self.game_frames.append(
+                    GameFrame(master=self, frame_colour=d, num_cols=cols, num_rows=rows)
+                )
+            #place each game frame
+            gf_size = (self.winfo_screenwidth() - 200) / self.num_game_frames
+            for i, gf in enumerate(self.game_frames):
+                self.columnconfigure(1+i, minsize=gf_size, weight=0)
+                gf.grid(row=0, column=1+i, sticky="nsew")
+
+            game_type = self.main_menu.game_selection.get()
+            #make the game
+            self.controller.new_game(num_cols=cols, num_rows=rows)
+            match game_type:
+                case "Player vs Player":
+                    logging.debug("Player vs Player game created")
+                case "Player vs Agent":
+                    agent_settings = self.main_menu.agent_settings_1
+                    agent_settings["type"] = self.main_menu.agent_selection_1.get()
+                    agent_settings["colour"] = self.main_menu.agent_colour.get()
+                    self.controller.new_pva_game(agent_settings)
+                    logging.debug("Player vs Agent game created")
+                case "Agent vs Agent":
+                    agent_1_settings = self.main_menu.agent_settings_1
+                    agent_1_settings["colour"] = self.main_menu.agent_colour.get()
+                    agent_1_settings["type"] = self.main_menu.agent_selection_1.get()
+                    agent_2_settings = self.main_menu.agent_settings_2
+                    agent_2_settings["colour"] = util.swap_colour(agent_1_settings["colour"])
+                    agent_2_settings["type"] = self.main_menu.agent_selection_2.get()
+                    iterations = self.main_menu.iterations
+                    #run the new game in the controller
+                    logging.debug("Agent vs Agent game created")
+                    self.controller.new_ava_game(agent_1_settings, agent_2_settings, iterations)
+
+
+    def kill_game(self):
+        """
+        Resets all game frames.
+        """
+        #reconfigure column sizes
         for i in range(3):
             self.columnconfigure(1+i, minsize=0, weight=0)
+        #remove the game frames
         for frame in self.game_frames:
             frame.grid_forget()
             frame.destroy()
+        #reset our list of game frames
         self.game_frames.clear()
-        #set the number of game frames to be created for correct sizing
-        self.num_game_frames = len(displays)
-        #create new game frames
-        for d in displays:
-            self.game_frames.append(
-                GameFrame(master=self, frame_colour=d, num_cols=cols, num_rows=rows)
-            )
-        #place each game frame
-        gf_size = (self.winfo_screenwidth() - 200) / self.num_game_frames
-        for i, gf in enumerate(self.game_frames):
-            self.columnconfigure(1+i, minsize=gf_size, weight=0)
-            gf.grid(row=0, column=1+i, sticky="nsew")
 
-        game_type = self.main_menu.game_selection.get()
-        #make the game
-        self.controller.new_game(num_cols=cols, num_rows=rows)
-        match game_type:
-            case "Player vs Player":
-                logging.debug("Player vs Player game created")
-            case "Player vs Agent":
-                agent_settings = self.main_menu.agent_settings_1
-                agent_settings["type"] = self.main_menu.agent_selection_1.get()
-                agent_settings["colour"] = self.main_menu.agent_colour.get()
-                self.controller.new_pva_game(agent_settings)
-                logging.debug("Player vs Agent game created")
-            case "Agent vs Agent":
-                agent_1_settings = self.main_menu.agent_settings_1
-                agent_1_settings["colour"] = self.main_menu.agent_colour.get()
-                agent_1_settings["type"] = self.main_menu.agent_selection_1.get()
-                agent_2_settings = self.main_menu.agent_settings_2
-                agent_2_settings["colour"] = util.swap_colour(agent_1_settings["colour"])
-                agent_2_settings["type"] = self.main_menu.agent_selection_2.get()
-                iterations = self.main_menu.iterations
-                #run the new game in the controller
-                logging.debug("Agent vs Agent game created")
-                self.controller.new_ava_game(agent_1_settings, agent_2_settings, iterations)
+
+    def toggle_training(self):
+        """
+        Toggle the training mode on/off.
+        Display the training menu where the game usually is when we turn it on,
+        Remove the training menu when we turn it off
+        """
+        if self.training_mode:
+            #remove frame safely
+            self.columnconfigure(1, minsize=0, weight=0)
+            self.training_frame.grid_forget()
+            self.training_frame.destroy()
+            self.training_frame = None
+        else:
+            #remove any games
+            self.kill_game()
+            #create training frame and display
+            self.columnconfigure(1, minsize=self.winfo_screenwidth() - 200, weight=0)
+            self.training_frame = TrainingFrame(self)
+            self.training_frame.grid(row=0, column=1, sticky="nsew")
+        self.training_mode = not self.training_mode
 
 
     def win_game(self, colour: str):
@@ -162,6 +197,7 @@ class MainMenuFrame(tk.Frame):
         # define widgets for main menu frame
         # new game button
         btn_newgame = tk.Button(self, text="New Game", command=master.new_game)
+        btn_training = tk.Button(self, text="Training", command=master.toggle_training)
 
         # row changing
         frm_rows = tk.Frame(self)
@@ -171,7 +207,7 @@ class MainMenuFrame(tk.Frame):
             lbl_init_val=str(self.rows),
             update="rows"
         )
-    
+
         #column changing
         frm_cols = tk.Frame(self)
         self.add_parameter(
@@ -262,6 +298,7 @@ class MainMenuFrame(tk.Frame):
 
         # place widgets into menu frame
         btn_newgame.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
+        btn_training.grid(row=1, column=0, sticky="ew", padx=5, pady=5)
         frm_rows.grid(row=2, column=0, sticky="ew", padx=5, pady=5)
         frm_cols.grid(row=4, column=0, sticky="ew", padx=5, pady=5)
         lbl_displays.grid(row=5, column=0, sticky="ew", padx=5, pady=5)
@@ -513,6 +550,17 @@ class MainMenuFrame(tk.Frame):
                     tk_var=settings["learning"]
                 )
                 frm_learning.grid(row=5, sticky="ew")
+
+
+class TrainingFrame(tk.Frame):
+    """
+    Frame where we can initiate simulations between agents, and display the results.
+    """
+    def __init__(self, master: DisplayWindow):
+        super().__init__(master=master, relief=tk.RAISED, bd=2)
+        #add training frame contents
+        lbl_title = tk.Label(master=self, text="Training Frame")
+        lbl_title.grid(row=0, column=0, sticky="n")
 
 
 class GameFrame(tk.Frame):
